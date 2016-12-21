009d3c935d62777a74f4416c347d286370be9b44
Kevin Rogers <kevin@KevinRogers.org>
2016-12-20 20:27:53 -0500

    Added simple blink to test builtin LED

--- Arduino/TestCode/BlueBox-Arduino-Test/BlueBox-Arduino-Test.ino
+++ Arduino/TestCode/BlueBox-Arduino-Test/BlueBox-Arduino-Test.ino
@@ -11,11 +11,14 @@
 #include "BluefruitConfig.h"
 
 void setup() {
-  // put your setup code here, to run once:
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for a second
 }
