3daa864c78dc7761ce81405e43c16b426db974d7
Kevin Rogers <kevin@KevinRogers.org>
2016-12-20 19:32:06 -0500

    Adding Arduino test stub

---
+++ Arduino/TestCode/BlueBox-Arduino-Test/BlueBox-Arduino-Test.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+  #include <SoftwareSerial.h>
+#endif
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
